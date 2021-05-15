970acbacde1b49429d09ee50483c01bde22860d6
blck-snwmn <whitesnowmancreator@gmail.com>
2021-05-16 05:48:09 +0900

    Parse section

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,21 @@
+use crate::decode;
+use std::{
+    convert::TryFrom,
+    io::{Cursor, Seek, SeekFrom},
+};
+use thiserror::Error;
+#[derive(Error, Debug)]
+pub(crate) enum ParseError {
+    #[error("faild to decode: {0}")]
+    Decode(#[from] decode::DecodeError),
+    #[error("faild to convert: {0}")]
+    Convert(#[from] std::num::TryFromIntError),
+    #[error("read failed: {0}")]
+    Read(#[from] std::io::Error),
+}
+
+type Result<T> = std::result::Result<T, ParseError>;
+
 // uintN
 pub(crate) type uint8 = u8;
 pub(crate) type uint32 = u32;
@@ -13,30 +31,61 @@ pub(crate) type varint7 = i8;
 pub(crate) type varint32 = i32;
 pub(crate) type varint64 = i64;
 
-pub struct Module<'a> {
+pub struct Module {
     pub magic_number: uint32,
     pub version: uint32,
-    pub sections: Option<&'a [Section]>,
+    pub sections: Vec<Section>,
 }
 
-impl<'a> Module<'a> {
-    pub(crate) fn new(
-        magic_number: uint32,
-        version: uint32,
-        sections: Option<&'a [Section]>,
-    ) -> Self {
+impl Module {
+    pub(crate) fn new(magic_number: uint32, version: uint32, sections: Vec<Section>) -> Self {
         Self {
             magic_number,
             version,
             sections,
         }
     }
+
+    pub(crate) fn parse(data: &mut Cursor<&[u8]>) -> Result<Self> {
+        let magic_number = u32::from_le_bytes(decode::decode_32bit(data)?);
+        let version = u32::from_le_bytes(decode::decode_32bit(data)?);
+        let sections = Section::parse_multi(data)?;
+        Ok(Self::new(magic_number, version, sections))
+    }
 }
 
 pub struct Section {
-    id: varuint7,
-    payload_len: varuint32,
-    payload_data: SectionData,
+    pub id: varuint7,
+    pub payload_len: varuint32,
+    pub payload_data: Vec<u8>,
+}
+
+impl Section {
+    pub(crate) fn parse(data: &mut Cursor<&[u8]>) -> Result<Self> {
+        let id = u8::try_from(decode::decode_varint(data)?)?;
+
+        let payload_len = u32::try_from(decode::decode_varint(data)?)?;
+
+        let payload_data = decode::decode_len(data, payload_len as usize)?;
+
+        Ok(Self {
+            id,
+            payload_len,
+            payload_data,
+        })
+    }
+
+    pub(crate) fn parse_multi(data: &mut Cursor<&[u8]>) -> Result<Vec<Self>> {
+        let current_position = data.position();
+        let end = data.seek(SeekFrom::End(0))?;
+        data.seek(SeekFrom::Start(current_position))?;
+        let mut v = Vec::new();
+        while end > data.position() {
+            let value = Self::parse(data)?;
+            v.push(value);
+        }
+        Ok(v)
+    }
 }
 
 pub enum SectionData {
@@ -50,4 +99,49 @@ pub enum SectionData {
     Element,
     Code,
     Data,
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    #[test]
+    fn test_parse_module() {
+        {
+            let min_input: &[u8] = &[0x00, 0x61, 0x73, 0x6D, 0x01, 0x00, 0x00, 0x00];
+            let min_input = &mut Cursor::new(min_input);
+            let result = Module::parse(min_input);
+            if let Err(e) = &result {
+                println!("{:?}", e);
+            }
+            assert!(result.is_ok());
+            let result = result.unwrap();
+            assert_eq!(result.magic_number, 0x6d736100);
+            assert_eq!(result.version, 1);
+            assert!(result.sections.is_empty());
+            let current = min_input.position();
+            let end = min_input.seek(SeekFrom::End(0)).unwrap();
+            assert_eq!(current, end);
+        }
+        {
+            let input: &[u8] = &[
+                0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, // magic number, version
+                0x01, 0x05, 0x01, 0x60, 0x00, 0x01, 0x7f, // type section
+                0x03, 0x02, 0x01, 0x00, // function section
+                0x0a, 0x06, 0x01, 0x04, 0x00, 0x41, 0x2a, 0x0b, // code section
+                0x00, 0x12, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x01, 0x06, 0x01, 0x00, 0x03, 0x61, 0x64,
+                0x64, 0x02, 0x03, 0x01, 0x00, 0x00, // custom section
+            ];
+            let input = &mut Cursor::new(input);
+            let result = Module::parse(input);
+            if let Err(e) = &result {
+                println!("{:?}", e);
+            }
+            assert!(result.is_ok());
+            let result = result.unwrap();
+            assert_eq!(result.magic_number, 0x6d736100);
+            assert_eq!(result.version, 1);
+            assert!(!result.sections.is_empty());
+        }
+    }
 }
--- src/decode.rs
+++ src/decode.rs
@@ -1,34 +1,28 @@
-use std::io::{Cursor, Read};
-
 use thiserror::Error;
 
-use crate::ast;
+use crate::ast::{self, Section};
 #[derive(Error, Debug)]
-enum DecodeError {
+pub(crate) enum DecodeError {
     #[error("unexpected format. end come after MSB is 0")]
     UnexpectFormat,
-    #[allow(dead_code)]
-    #[error("unexpected red size. got={0}, want={1}")]
-    UnexpectRepeatSize(u128, u128),
     #[error("no expected type value. got={0}")]
     UnexpectedWireDataValue(u128),
-    #[error("")]
+    #[error("read failed: {0}")]
     FailedToRead(#[from] std::io::Error),
 }
 
 type Result<T> = std::result::Result<T, DecodeError>;
 
-fn decode_varints<T: std::io::Read>(data: &mut T) -> Result<u64> {
+pub(crate) fn decode_varint<T: std::io::Read>(data: &mut T) -> Result<u64> {
     // とりあえずu64で読んで、その後工程で varuint32へ変換できるかどうか見る方針
 
-    // iterate take_util とかでもできるよ
     let mut sum = 0;
     let mut loop_count = 0;
     loop {
         let mut buf = [0; 1];
         let result = data.read_exact(&mut buf);
         if result.is_err() {
-            return Err(DecodeError::UnexpectFormat);
+            break Err(DecodeError::UnexpectFormat);
         }
         // MSB は後続のバイトが続くかどうかの判定に使われる
         // 1 の場合、後続が続く
@@ -39,9 +33,15 @@ fn decode_varints<T: std::io::Read>(data: &mut T) -> Result<u64> {
         sum += buf;
         loop_count += 1;
         if top != 0b10000000 {
-            return Ok(sum);
+            break Ok(sum);
         }
     }
+}
+
+pub(crate) fn decode_len<T: std::io::Read>(data: &mut T, len: usize) -> Result<Vec<u8>> {
+    let mut buf = vec![0; len];
+    data.read_exact(&mut buf)?;
+    Ok(buf)
 }
 
 fn decode_nbit<T: std::io::Read, const SIZE: usize>(data: &mut T) -> Result<[u8; SIZE]> {
@@ -50,34 +50,12 @@ fn decode_nbit<T: std::io::Read, const SIZE: usize>(data: &mut T) -> Result<[u8;
     Ok(buf)
 }
 
-fn decode_8bit<T: std::io::Read>(data: &mut T) -> Result<[u8; 1]> {
+pub(crate) fn decode_8bit<T: std::io::Read>(data: &mut T) -> Result<[u8; 1]> {
     decode_nbit(data)
 }
-fn decode_16bit<T: std::io::Read>(data: &mut T) -> Result<[u8; 2]> {
+pub(crate) fn decode_16bit<T: std::io::Read>(data: &mut T) -> Result<[u8; 2]> {
     decode_nbit(data)
 }
-fn decode_32bit<T: std::io::Read>(data: &mut T) -> Result<[u8; 4]> {
+pub(crate) fn decode_32bit<T: std::io::Read>(data: &mut T) -> Result<[u8; 4]> {
     decode_nbit(data)
-}
-fn decode_module<T: std::io::Read>(data: &mut T) -> Result<ast::Module> {
-    let magic_number = u32::from_le_bytes(decode_32bit(data)?);
-    let version = u32::from_le_bytes(decode_32bit(data)?);
-    Ok(ast::Module::new(magic_number, version, None))
-}
-#[cfg(test)]
-mod test {
-
-    use super::*;
-    #[test]
-    fn read_module() {
-        {
-            let mut min_input: &[u8] = &[0x00, 0x61, 0x73, 0x6D, 0x01, 0x00, 0x00, 0x00];
-            let result = decode_module(&mut min_input);
-            assert!(result.is_ok());
-            let result = result.unwrap();
-            assert_eq!(result.magic_number, 0x6d736100);
-            assert_eq!(result.version, 1);
-            assert!(result.sections.is_none());
-        }
-    }
 }
